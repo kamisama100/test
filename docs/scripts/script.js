fba9501f80e9ecc62df610012d90ee0de6c32d71
Lee Johnder Delute <kamisama100>
2024-02-07 13:28:02 +0800

    updates

--- docs/scripts/script.js
+++ docs/scripts/script.js
@@ -1,3 +1,59 @@
+let publicationTime = "12:00 AM"
+let publicationTimezone = "EST"
+
+const toggleHidden = (el) => {
+  el.classList.toggle("hidden")
+  console.log(el)
+}
+
+const openDropDown = (dropdown) => {
+  dropdown.classList.replace("max-h-0", "max-h-dropdown")
+  dropdown.classList.replace("opacity-0", "opacity-100")
+  dropdown.classList.replace("pointer-events-none", "pointer-events-auto")
+}
+
+const toggleOnInput = (input, el) => {
+  if (input.value.trim() === "") {
+    el.style.display = "inline"
+  } else {
+    el.style.display = "none"
+  }
+}
+
+const selectInput = (value, input, dropdown) => {
+  let formattedValue = value.trim()
+  const datePicker = document.querySelector("#datepicker")
+
+  if (formattedValue.includes("AM") || formattedValue.includes("PM")) {
+    publicationTime = formattedValue
+    input.value = publicationTime
+    if (datePicker.value !== "") {
+      let dateValueArr = datePicker.value.split(" ")
+      let timeArr = [dateValueArr[1], dateValueArr[2]].join(" ")
+      timeArr = publicationTime
+
+      dateValueArr[1] = timeArr
+      dateValueArr.splice(2, 1)
+
+      datePicker.value = dateValueArr.join(" ")
+    }
+  } else {
+    publicationTimezone = formattedValue
+    input.value = publicationTimezone
+    if (datePicker.value !== "") {
+      let dateValueArr = datePicker.value.split(" ")
+      dateValueArr[3] = publicationTimezone
+      let newValue = dateValueArr.join(" ")
+      datePicker.value = newValue
+    }
+  }
+
+  console.log(dropdown)
+  dropdown.classList.replace("max-h-dropdown", "max-h-0")
+  dropdown.classList.replace("opacity-100", "opacity-0")
+  dropdown.classList.replace("pointer-events-auto", "pointer-events-none")
+}
+
 document.addEventListener("DOMContentLoaded", function () {
   const moveAdditionalInput = () => {
     const datepickerContainer = document.querySelector(".air-datepicker")
@@ -14,23 +70,63 @@ document.addEventListener("DOMContentLoaded", function () {
 
   new AirDatepicker("#datepicker", {
     locale: localeEn,
-    dateFormat: "yyyy-mm-dd",
-    offset: 2,
+    offset: 3,
     firstDay: 6,
-    visible: true,
+    multipleDates: false,
     onShow: moveAdditionalInput,
     onHide: moveAdditionalInputBack,
+    onSelect: function () {
+      datepicker.value = `${datepicker.value} ${publicationTime} ${publicationTimezone}`
+      toggleOnInput(datepicker, document.querySelector("#placeholder-star"))
+    },
   })
 })
 
-const toggleHidden = (el) => {
-  el.classList.toggle("hidden")
+const dropdowns = document.querySelectorAll("#dropdown")
+
+document.addEventListener("click", (event) => {
+  dropdowns.forEach((e) => {
+    const inputEl = e.parentElement.parentElement.querySelector("input")
+
+    if (!e.contains(event.target) && !inputEl.contains(event.target)) {
+      e.classList.replace("max-h-dropdown", "max-h-0")
+      e.classList.replace("opacity-100", "opacity-0")
+      e.classList.replace("pointer-events-auto", "pointer-events-none")
+    }
+  })
+})
+
+const generateTimeList = () => {
+  let timeList = []
+  for (let hours = 0; hours < 24; hours++) {
+    for (let minutes = 0; minutes < 60; minutes += 30) {
+      let hour = hours % 12 === 0 ? 12 : hours % 12
+      let amPm = hours < 12 ? "AM" : "PM"
+      let hourString = hour.toString().padStart(2, "0")
+      let minuteString = minutes.toString().padStart(2, "0")
+      timeList.push(`${hourString}:${minuteString} ${amPm}`)
+    }
+  }
+  return timeList
 }
 
-const toggleOnInput = (input, el) => {
-  if (input.value.trim() === "") {
-    el.style.display = "inline"
-  } else {
-    el.style.display = "none"
-  }
+const createDropdownButtons = () => {
+  const dropdown = document.getElementById("dropdown")
+  const timeList = generateTimeList()
+
+  timeList.forEach((time) => {
+    const button = document.createElement("button")
+    button.className = "hover:bg-[#bb1919] hover:text-white p-1.5"
+    button.textContent = time
+    button.onclick = function () {
+      selectInput(
+        this.textContent,
+        this.parentElement.parentElement.querySelector("input"),
+        this.parentElement
+      )
+    }
+    dropdown.appendChild(button)
+  })
 }
+
+createDropdownButtons()
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,12 @@ module.exports = {
       boxShadow: {
         bottom: "-5px 16px 16px #d6cfcfb8",
       },
+      borderColor: {
+        primary: "#c6c3c396",
+      },
+      opacity: {
+        100: "100",
+      },
     },
   },
   plugins: [],
